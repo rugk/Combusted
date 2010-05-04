fa2f9c00d282d19a87ce247ed36b61c42f672087
silverwind <silverwind@gmail.com>
2010-05-04 13:41:53 +0200

    Way better approach hooking the Cooldown API. This should work with all barmods using the default 120 buttons.

--- Combusted.lua
+++ Combusted.lua
@@ -1,27 +1,32 @@
-local cds, cd = {}, nil
-local kill = function() return end
-
+local start, ids, _G = nil, {}, _G
 local f = CreateFrame"Frame"
 f:RegisterEvent"COMBAT_LOG_EVENT_UNFILTERED"
 f:RegisterEvent"PLAYER_ENTERING_WORLD"
+f:RegisterEvent"ACTIONBAR_SLOT_CHANGED"
 f:SetScript("OnEvent", function(self, event, ...)
-	if event == "PLAYER_ENTERING_WORLD" then
-		for i=1,120 do
-			cd = _G["BT4Button"..i.."Cooldown"]
-			if cd then
-				if cd:GetParent().icon:GetTexture() == "Interface\\Icons\\Spell_Fire_SealOfFire" then
-					cds[#cds+1] = cd
-				end
-			end
-		end
-		self:UnregisterEvent"PLAYER_ENTERING_WORLD"
-	else
+	if event == "COMBAT_LOG_EVENT_UNFILTERED" then
 		if arg9 ~= 28682 then return end
-		if arg2 == "SPELL_AURA_REMOVED" and arg6 == UnitGUID"player" then
-			for k,v in pairs(cds) do
-				v:SetCooldown(GetTime(),120)
-				v.Show, v.Hide = kill, kill
+		if arg2 == "SPELL_AURA_REMOVED" and arg6 == _G.UnitGUID"player" then
+				start = _G.GetTime()
+		end
+	else
+		for i=1,120 do
+			if select(4,_G.GetActionInfo(i)) == 28682 then
+				ids[i] = true
+			else
+				ids[i] = false
 			end
 		end
 	end
 end)
+
+local oldGetActionCooldown = GetActionCooldown
+function GetActionCooldown(...)
+	local slot = ...
+	if not start then return oldGetActionCooldown(...) end
+	if ids[slot] and (start + 120 > _G.GetTime()) then
+		return start, 120, 1
+	else
+		return oldGetActionCooldown(...)
+	end
+end
